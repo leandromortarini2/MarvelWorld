d9d2423b5f9149c9b9171575be8c61ee08483933
leandro mortarini <leandromortarini2@gmail.com>
2024-06-10 21:03:51 -0300

    Inicio de MarvelWorld

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Personajes } from "./components/Personajes/Personajes";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>MARVEL WORLD</h1>
+      </div>
+      <Personajes />
+    </>
+  );
+}
+
+export default App;
