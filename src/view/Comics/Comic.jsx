2c0ee7d9ef5c689ca27b8119fd5ac3b747f4bfd4
leandro mortarini <leandromortarini2@gmail.com>
2024-06-13 09:49:38 -0300

    estilos en comic

--- src/view/Comics/Comic.jsx
+++ src/view/Comics/Comic.jsx
@@ -21,23 +21,34 @@ export const Comic = () => {
   }, []);
 
   return (
-    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-gray-200 pb-5">
-      <h1 className="xl:text-[100px] text-red-800 font-bold uppercase tracking-wider opacity-50 ">
-        comic
-      </h1>
-      <div className="w-full flex flex-wrap justify-evenly">
+    <div className="w-full min-h-screen flex flex-col items-center bg-white   pb-5">
+      <div className="w-full h-[300px]  flex flex-col items-center  ">
+        <h1 className="absolute text-white text-[200px] uppercase top-32 font-bold opacity-100 ">
+          comic
+        </h1>
+        <img
+          className=" xl:h-[600px]  "
+          src="https://cdn.marvel.com/content/1x/mi_wallpaper_mas_dsk_01.jpg"
+          alt=""
+        />
+      </div>
+
+      <div className="w-full flex flex-wrap justify-center ">
         {comics.length > 0
           ? comics.map((element) => {
               return (
                 <div
                   key={element.id}
-                  className=" min-w-1/4 h-[270px]  m-2 flex  items-center  bg-white rounded-xl"
+                  className="xl:w-1/6  m-1 flex flex-col items-center bg-white rounded-t-xl border-4 border-white hover:scale-110 duration-1000  "
                 >
                   <img
-                    className="w-full h-full rounded-xl"
+                    className="w-full h-full rounded-t-xl"
                     src={`${element.thumbnail.path}.${element.thumbnail.extension}`}
                     alt=""
                   />
+                  <button className="w-full  h-12 bg-red-500 text-white rounded-b-xl hover:bg-red-800 capitalize ">
+                    see comic
+                  </button>
                 </div>
               );
             })
