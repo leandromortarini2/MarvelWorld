75207c2793fb13d76f98eca31454ec3b65698144
leandro mortarini <leandromortarini2@gmail.com>
2024-06-18 09:37:03 -0300

    estilos detalle de Comic

--- src/view/ComicDetail/ComicDetail.jsx
+++ src/view/ComicDetail/ComicDetail.jsx
@@ -55,40 +55,64 @@ export const ComicDetail = () => {
 
   return (
     <>
-      <div className="w-full min-h-screen flex flex-col items-center pb-5">
+      <div className="w-full min-h-screen flex flex-col justify-center items-center  pb-5">
         {comic?.length > 0
           ? comic?.map((element) => {
               return (
-                <div key={element.id} className="w-3/4 min-h-screen flex ">
+                <div
+                  key={element.id}
+                  className="w-3/4 min-h-screen flex justify-center  mt-5 "
+                >
                   {/* container 1 */}
                   {/* container 1 */}
-                  <div className="w-1/2 min-h-screen flex justify-center items-center">
+                  <div className="w-1/3 min-h-screen flex justify-center items-center">
                     <img
-                      className="xl:h-[500px] shadow-black shadow-2xl "
+                      className="xl:h-[600px] shadow-black shadow-2xl "
                       src={`${element.thumbnail.path}.${element.thumbnail.extension}`}
                       alt=""
                     />
                   </div>
                   {/* container 2  */}
                   {/* container 2  */}
-                  <div className="w-1/2 flex flex-col justify-center items-center ">
-                    <div className="w-full h-32  rounded-t-2xl flex ">
-                      <h1 className="text-3xl font-bold text-justify text-red-900">
+                  <div className="w-1/2  flex flex-col justify-center items-center ml-5   ">
+                    <div className="w-full  h-26   flex p-4  bg-red-800">
+                      <h1 className="text-2xl font-bold text-center text-white">
                         {element.title}
                       </h1>
                     </div>
 
-                    <div className="w-full h-[300px]  flex flex-col justify-evenly ">
-                      <h3 className="text-3xl font-bold uppercase text-red-900">
-                        Creators
-                      </h3>
+                    <h3 className="text-3xl font-bold uppercase text-red-900 text-center mt-5 ">
+                      Creators
+                    </h3>
+                    <div className="w-3/4  flex flex-wrap mt-5   ">
                       {creator?.length > 0
                         ? creator?.map((creatorElement) => {
                             return (
                               <div key={creatorElement.id}>
-                                <p className="text-gray-800 font-bold text-xl">
-                                  {creatorElement.fullName}
+                                <p className="text-gray-700 font-semibold text-xl text-center p-2 ">
+                                  {`${creatorElement.fullName}, `}
                                 </p>
+                              </div>
+                            );
+                          })
+                        : null}
+                    </div>
+                    <h3 className="text-3xl font-bold uppercase text-red-900 text-center m-5 ">
+                      Characters
+                    </h3>
+                    <div className="w-full  flex justify-evenly flex-wrap">
+                      {characters?.length > 0
+                        ? characters.map((characterElement) => {
+                            return (
+                              <div key={characterElement.id}>
+                                <Link
+                                  to={`/CharacterDetail/${characterElement.id}`}
+                                >
+                                  <img
+                                    className="w-20 h-20 shadow-lg hover:scale-110 duration-1000 m-2"
+                                    src={`${characterElement.thumbnail.path}.${characterElement.thumbnail.extension}`}
+                                  />
+                                </Link>
                               </div>
                             );
                           })
@@ -99,22 +123,6 @@ export const ComicDetail = () => {
               );
             })
           : null}
-        <div className="w-3/4  flex justify-evenly flex-wrap">
-          {characters?.length > 0
-            ? characters.map((characterElement) => {
-                return (
-                  <div key={characterElement.id}>
-                    <Link to={`/CharacterDetail/${characterElement.id}`}>
-                      <img
-                        className="w-60 h-60 rounded-lg m-2 shadow-lg shadow-black hover:scale-110 duration-1000"
-                        src={`${characterElement.thumbnail.path}.${characterElement.thumbnail.extension}`}
-                      />
-                    </Link>
-                  </div>
-                );
-              })
-            : null}
-        </div>
       </div>
     </>
   );
