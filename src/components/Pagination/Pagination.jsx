bce6d61e9a8b77bb2a1825f942e40c16a05b8d84
leandro mortarini <leandromortarini2@gmail.com>
2024-06-18 14:41:08 -0300

    paginacion comic

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { useState } from "react";
+
+export const Pagination = ({
+  comicsPerPage,
+  currentPage,
+  setCurrentPage,
+  totalComics,
+}) => {
+  const pageNumbers = [];
+
+  console.log(Math.ceil(totalComics / comicsPerPage));
+
+  for (let i = 1; i <= Math.ceil(totalComics / comicsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const handlePreviusPage = (event) => {
+    event.preventDefault();
+    setCurrentPage(currentPage - 1);
+  };
+  const handleNextPage = (event) => {
+    event.preventDefault();
+
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handleSpecificPage = (numberPage) => {
+    setCurrentPage(numberPage);
+  };
+
+  return (
+    <nav
+      className="pagination is-centered"
+      role="navigation"
+      aria-label="pagination"
+    >
+      <a
+        onClick={handlePreviusPage}
+        href="#"
+        className={`pagination-previous bg-red-800 border-none  ${
+          currentPage === 1 ? "is-disabled" : ""
+        }`}
+      >
+        Previous
+      </a>
+      <a
+        onClick={handleNextPage}
+        href="#"
+        className={`pagination-next bg-red-800 border-none ${
+          currentPage >= pageNumbers.length ? "is-disabled" : ""
+        }`}
+      >
+        Next
+      </a>
+      <ul className="pagination-list">
+        {pageNumbers?.map((element) => {
+          return (
+            <li key={element}>
+              <a
+                href="#"
+                className={`pagination-link text-red-900 border-red-800 ${
+                  element === currentPage
+                    ? "is-current bg-red-800 text-gray-100 "
+                    : ""
+                } `}
+                onClick={() => handleSpecificPage(element)}
+              >
+                {element}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
--- src/view/Comics/Comic.jsx
+++ src/view/Comics/Comic.jsx
@@ -3,9 +3,17 @@ import React, { useEffect, useState } from "react";
 import { getComics } from "../../helpers/solicitudes";
 import { Link } from "react-router-dom";
 import logoSuper from "../../assets/capitan.svg";
+import { Pagination } from "../../components/Pagination/Pagination";
 
 export const Comic = () => {
   const [comics, setComics] = useState([]);
+  const [comicsPerPage, setComicsPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalComics = comics.length;
+
+  const lastIndex = currentPage * comicsPerPage;
+  const firstIndex = lastIndex - comicsPerPage;
 
   useEffect(() => {
     try {
@@ -29,13 +37,6 @@ export const Comic = () => {
   return (
     <div className="w-full min-h-screen flex flex-col items-center bg-white  pb-5">
       <div className="w-full h-[400px]  flex flex-col items-center  ">
-        <div className="w-1/3 h-[100px] bg-red-900 absolute flex justify-center items-center xl:top-80 rounded-full border-4 border-white shadow-white shadow-2xl opacity-90">
-          <img className="w-20" src={logoSuper} alt="" />{" "}
-          <h1 className=" text-white text-[100px] uppercase font-bold  ">
-            comic
-          </h1>
-        </div>
-
         <img
           className="w-full xl:h-[400px] shadow-lg shadow-gray-800 "
           src="https://cdn.marvel.com/content/1x/mi_wallpaper_mas_dsk_01.jpg"
@@ -43,35 +44,51 @@ export const Comic = () => {
         />
       </div>
 
-      <div className="w-full flex flex-wrap justify-center ">
-        {comics.length > 0
-          ? comics.map((element) => {
-              return (
-                <div
-                  key={element.id}
-                  className="xl:w-1/6 h-92 m-4 flex flex-col items-center bg-white rounded-xl  hover:scale-110 hover:shadow-red-800 hover:shadow-md duration-1000 overflow-hidden shadow-xl shadow-black border-4 border-gray-400 "
-                >
-                  <div className="w-full  h-20 bg-white text-red-700 capitalize  flex justify-center items-center">
-                    <h2 className="text-center font-semibold p-1">
-                      {element.title}
-                    </h2>
-                  </div>
+      <div className="w-full flex flex-col flex-wrap justify-center items-center ">
+        <div className="w-1/3 h-[100px] bg-red-900  flex justify-center items-center rounded-full border-4 border-white shadow-white shadow-2xl opacity-90 m-10">
+          <img className="w-20" src={logoSuper} alt="" />{" "}
+          <h1 className=" text-white text-[100px] uppercase font-bold  ">
+            comic
+          </h1>
+        </div>
+        <div className="w-full flex flex-wrap justify-center ">
+          {comics.length > 0
+            ? comics
+                .map((element) => {
+                  return (
+                    <div
+                      key={element.id}
+                      className="xl:w-1/6 h-92 m-4 flex flex-col items-center bg-white rounded-lg  hover:scale-110 hover:shadow-black hover:shadow-2xl duration-1000 overflow-hidden shadow-lg shadow-gray-400 "
+                    >
+                      <div className="w-full  h-20 bg-white text-red-700 capitalize  flex justify-center items-center">
+                        <h2 className="text-center font-semibold p-1">
+                          {element.title}
+                        </h2>
+                      </div>
 
-                  <img
-                    className="w-full h-72"
-                    src={`${element.thumbnail.path}.${element.thumbnail.extension}`}
-                    alt=""
-                  />
-                  <Link to={`/ComicDetail/${element.id}`}>
-                    <button className="w-60 m-2  h-10 bg-red-900 text-white duration-1000   capitalize font-semibold text-xl rounded-lg  hover:bg-red-500">
-                      see comic
-                    </button>
-                  </Link>
-                </div>
-              );
-            })
-          : null}
+                      <img
+                        className="w-full h-72"
+                        src={`${element.thumbnail.path}.${element.thumbnail.extension}`}
+                        alt=""
+                      />
+                      <Link to={`/ComicDetail/${element.id}`}>
+                        <button className="w-60 m-2  h-10 bg-red-900 text-white duration-1000   capitalize font-semibold text-xl rounded-lg  hover:bg-red-500">
+                          see comic
+                        </button>
+                      </Link>
+                    </div>
+                  );
+                })
+                .slice(firstIndex, lastIndex)
+            : null}
+        </div>
       </div>
+      <Pagination
+        comicsPerPage={comicsPerPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalComics={totalComics}
+      />
     </div>
   );
 };
