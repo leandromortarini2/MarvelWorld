5759f78386744c1db933abb02e1c5b85467c6660
leandro mortarini <leandromortarini2@gmail.com>
2024-06-21 17:08:18 -0300

    avances en detail character

--- src/view/CharacterDetail/CharacterDetail.jsx
+++ src/view/CharacterDetail/CharacterDetail.jsx
@@ -18,8 +18,30 @@ export const CharacterDetail = () => {
   const [stories, setStories] = useState(null);
   const [nameHero, setnameHero] = useState(null);
 
+  // BUTTONS
+
+  const [buttonComic, setButtonComic] = useState(true);
+  const [buttonEvents, setButtonEvents] = useState(false);
+  const [buttonSeries, setButtonSeries] = useState(false);
+  const [buttonStories, setButtonStories] = useState(false);
+
+  // const [buttonSelect, setButtonSelect] = useState(false);
+
+  const handleButtonComic = () => {
+    setButtonComic(!buttonComic);
+  };
+  const handleButtonEvents = () => {
+    setButtonEvents(!buttonEvents);
+  };
+  const handleButtonSeries = () => {
+    setButtonSeries(!buttonSeries);
+  };
+  const handleButtonStorie = () => {
+    setButtonStories(!buttonStories);
+  };
+
   // paginacion comic
-  const [comicsPerPage, setComicsPerPage] = useState(9);
+  const [comicsPerPage, setComicsPerPage] = useState(4);
   const [currentPage, setCurrentPage] = useState(1);
 
   const totalComics = comics?.length;
@@ -52,7 +74,7 @@ export const CharacterDetail = () => {
     try {
       const fetchData = async () => {
         const { results } = await getCharacterComic(id);
-        console.log(".................................", results);
+        // console.log(".................................", results);
         setComics(results);
       };
       fetchData();
@@ -111,189 +133,121 @@ export const CharacterDetail = () => {
 
   return (
     <>
-      <div className="w-full  min-h-screen flex flex-col  justify-start items-center bg-gray-800 ">
-        {" "}
-        {/* personaje */}
-        {/* personaje */}
-        {/* personaje */}
-        {/* personaje */}
-        {/* personaje */}
-        {/* personaje */}
-        {/* personaje */}
-        {character?.length > 0
-          ? character.map((character) => {
-              return (
-                <div
-                  key={character.id}
-                  className="w-full  flex flex-col items-center justify-center "
-                >
-                  <div className="w-full  bg-Marvel flex flex-col items-center justify-center p-4">
-                    <img
-                      className="w-52 ShadowEffect2 rounded-full mr-5 border-4 border-white"
-                      src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                    />{" "}
-                    <h1 className="text-6xl font-bold text-center text-white ShadowEffect2">
-                      {character.name}
-                    </h1>
-                  </div>
-                  {/* comic 2  */}
-                  {/* comic 2  */}
-                  {/* comic 2  */}
-                  {/* <div className="w-full   p-2 flex flex-col items-center justify-start   ShadowEffect2   ">
-                    <div className="w-full md:w-3/4 lg: lg:w-1/4 flex flex-col md:flex-row justify-center items-center bg-Marvel  rounded-md md:p-2 m-2 ShadowEffect2">
-                      <div className="w-full md:w-3/4 h-3/4  flex flex-col justify-center items-center rounded-lg  ">
-                        <h2 className=" text-white text-3xl md:text-6xl lg:text-4xl xl:text-3xl uppercase font-bold rounded-lg ">
-                          comic
-                        </h2>
+      <div className="w-full  min-h-screen flex justify-start items-start bg-gray-800 ">
+        {/* CONTAINER IZQ */}
+        {/* CONTAINER IZQ */}
+        {/* CONTAINER IZQ */}
+        <div className="w-1/4 min-h-screen  bg-white">
+          <div className="w-full flex  justify-start items-start flex-wrap  ">
+            {/* personaje */}
+            {/* personaje */}
+            {character?.length > 0
+              ? character.map((character) => {
+                  return (
+                    <div
+                      key={character.id}
+                      className="w-full  flex flex-col items-center justify-center "
+                    >
+                      <div className="w-full  bg-Marvel flex flex-col items-center justify-center p-4">
+                        <img
+                          className="w-52 ShadowEffect2 rounded-full mr-5 border-4 border-white"
+                          src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                        />{" "}
+                        <h1 className="text-6xl font-bold text-center text-white ShadowEffect2">
+                          {character.name}
+                        </h1>
                       </div>
                     </div>
-                    <div className="w-full flex flex-wrap justify-evenly ">
-                      {comics?.length > 0
-                        ? comics
-                            .map((comic) => {
-                              return (
-                                <div key={comic.id} className=" min-h-60 p-2">
-                                  <img
-                                    className="h-52  shadow-lg hover:scale-110 duration-1000 m-2 ShadowEffect2"
-                                    src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
-                                  />
-                                </div>
-                              );
-                            })
-                            .slice(firstIndex, lastIndex)
-                        : null}
-                    </div>
-                    <Pagination
-                      comicsPerPage={comicsPerPage}
-                      currentPage={currentPage}
-                      setCurrentPage={setCurrentPage}
-                      totalComics={totalComics}
-                    />
-                  </div> */}
-                </div>
-              );
-            })
-          : null}
-        {/* container principal */}
-        {/* container principal */}
-        {/* container principal */}
-        <div className="w-full min-h-[250px] flex justify-evenly flex-wrap  ">
-          {/* container 1 */}
-          {/* container 1 */}
-          {/* container 1 */}
-          <div className="w-full md:w-3/4 lg: lg:w-1/3 md:h-[150px] flex flex-col md:flex-row justify-center items-center bg-Marvel  rounded-md md:p-5 m-10 ShadowEffect2">
-            {/*  */}
-            <div className="w-full md:w-1/3 flex items-center opacity-100 ">
-              <img
-                className="w-full xl:h-[200px] rounded-md ShadowEffect2"
-                src="https://www.imgworlds.com/_next/static/media/captain-america.e49a7de3.png"
-                alt=""
-              />
-            </div>{" "}
-            <div className="w-full md:w-3/4 h-3/4  flex flex-col justify-center items-center rounded-lg  ">
-              <h2 className=" text-white text-3xl md:text-6xl lg:text-4xl xl:text-5xl uppercase font-bold rounded-lg ">
-                comic{" "}
-              </h2>
-              <p className="text-white text-sm md:text-xl md:font-bold text-center">
-                find the most exciting stories{" "}
-              </p>
-            </div>
+                  );
+                })
+              : null}
           </div>
-          {/* container 2 */}
-          {/* container 2 */}
-          {/* container 2 */}
-          <div className="w-full md:w-3/4 lg: lg:w-1/3 md:h-[150px] flex flex-col md:flex-row justify-center items-center bg-Marvel  rounded-md md:p-5 m-10 ShadowEffect2">
-            {/*  */}
-            <div className="w-full md:w-1/3 flex items-center opacity-100 ">
-              <img
-                className="w-full xl:h-[200px] rounded-md ShadowEffect2"
-                src="https://www.imgworlds.com/_next/static/media/spiderman.8071ace6.png"
-                alt=""
-              />
-            </div>{" "}
-            <div className="w-full md:w-3/4 h-3/4  flex flex-col justify-center items-center rounded-lg  ">
-              <h2 className=" text-white text-3xl md:text-6xl lg:text-4xl xl:text-5xl uppercase font-bold rounded-lg ">
-                {" "}
-                Character
-              </h2>
-              <p className="text-white text-md md:font-bold text-center">
-                super heroes of the marvel world{" "}
-              </p>
-            </div>
-          </div>
-          {/* container 3 */}
-          {/* container 3 */}
-          {/* container 3 */}
-          <div className="w-full md:w-3/4 lg: lg:w-1/3 md:h-[150px] flex flex-col md:flex-row justify-center items-center bg-Marvel  rounded-md md:p-5 m-10 ShadowEffect2">
-            {/*  */}
-            <div className="w-full md:w-1/3 flex items-center opacity-100 ">
-              <img
-                className="w-full xl:h-[200px] rounded-md ShadowEffect2"
-                src="https://www.imgworlds.com/_next/static/media/iron-man.cbce2b80.png"
-                alt=""
-              />
-            </div>{" "}
-            <div className="w-full md:w-3/4 h-3/4  flex flex-col justify-center items-center rounded-lg  ">
-              <h2 className=" text-white text-3xl md:text-6xl lg:text-4xl xl:text-5xl uppercase font-bold rounded-lg ">
-                {" "}
-                stories
-              </h2>
-              <p className="text-white text-md md:font-bold text-center p-1">
-                original stories from the marvel world{" "}
-              </p>
-            </div>
-          </div>
-          {/* container 4 */}
-          {/* container 4 */}
-          {/* container 4 */}
-          <div className="w-full md:w-3/4 lg: lg:w-1/3 md:h-[150px] flex flex-col md:flex-row justify-center items-center bg-Marvel  rounded-md md:p-5 m-10 ShadowEffect2">
-            {/*  */}
-            <div className="w-full md:w-1/3 flex items-center opacity-100 ">
-              <img
-                className="w-full xl:h-[200px] rounded-md ShadowEffect2"
-                src="https://www.imgworlds.com/_next/static/media/hulk.4aaa2db7.png"
-                alt=""
-              />
-            </div>{" "}
-            <div className="w-full md:w-3/4 h-3/4  flex flex-col justify-center items-center rounded-lg  ">
-              <h2 className=" text-white text-3xl md:text-6xl lg:text-4xl xl:text-5xl uppercase font-bold rounded-lg ">
-                {" "}
-                Serie
-              </h2>
-              <p className="text-white text-md md:font-bold text-center p-1">
-                the series of your favorite heroes{" "}
-              </p>
-            </div>
-          </div>
-          {/* container 5 */}
-          {/* container 5 */}
-          {/* container 5 */}
-          <div className="w-full md:w-3/4 md:h-[150px] lg:h-[300px] flex flex-col md:flex-row justify-center items-center bg-Marvel  rounded-md p-5 m-10 ShadowEffect2">
-            {/*  */}
-            <div className="w-full md:w-1/4 flex items-center opacity-100 ">
-              <img
-                className="w-full lg:h-[300px] xl:h-[400px] rounded-md ShadowEffect2"
-                src="https://www.imgworlds.com/_next/static/media/thor.aabaca85.png"
-                alt=""
-              />
-            </div>{" "}
-            <div className="w-full md:w-3/4 h-3/4  flex flex-col justify-center items-center rounded-lg  ">
-              <h2 className=" text-white text-3xl md:text-6xl  xl:text-[70px] uppercase font-bold rounded-lg ">
-                Events
-              </h2>
-              <p className="text-white text-md lg:text-2xl xl:text-3xl md:font-bold text-center">
-                the series of your favorite heroes{" "}
-              </p>
-            </div>
-            <div className="hidden  w-1/4 lg:flex items-center opacity-100 ">
-              <img
-                className="lg:h-[300px] xl:h-[400px] rounded-md ShadowEffect2"
-                src="https://www.imgworlds.com/_next/static/media/hawkeye.a6d35081.png"
-                alt=""
-              />
-            </div>{" "}
+          {/* container buttons */}
+          {/* container buttons */}
+          {/* container buttons */}
+          <div className="w-full h-[400px] flex flex-col justify-evenly items-center flex-wrap bg-white  ">
+            <button
+              onClick={handleButtonComic}
+              className={`w-3/4 h-12  text-white font-bold xl:text-3xl uppercase tracking-wide duration-1000 hover:bg-Marvel rounded-lg ${
+                buttonComic === true ? "bg-Marvel" : " bg-gray-500"
+              }`}
+            >
+              comic
+            </button>
+            <button
+              onClick={handleButtonEvents}
+              className="w-3/4 h-12 bg-gray-500   text-white font-bold xl:text-3xl uppercase tracking-wide duration-1000 hover:bg-Marvel rounded-lg"
+            >
+              events
+            </button>
+            <button
+              onClick={handleButtonSeries}
+              className="w-3/4 h-12 bg-gray-500   text-white font-bold xl:text-3xl uppercase tracking-wide duration-1000 hover:bg-Marvel rounded-lg"
+            >
+              series
+            </button>
+            <button
+              onClick={handleButtonStorie}
+              className="w-3/4 h-12 bg-gray-500   text-white font-bold xl:text-3xl uppercase tracking-wide duration-1000 hover:bg-Marvel rounded-lg"
+            >
+              stories
+            </button>
           </div>
         </div>
+        {/* comic 2  */}
+        {/* comic 2  */}
+        {/* comic 2  */}
+        {buttonComic === true ? (
+          <div className="w-full min-h-screen mt-5 p-2 flex flex-col items-center justify-start   ShadowEffect2   ">
+            <div className="w-3/5 bg-Marvel flex justify-center items-center  ">
+              {" "}
+              <h2 className="text-white font-bold text-[80px] uppercase ShadowEffect2">
+                comic
+              </h2>
+            </div>
+
+            <div className="w-full min-h-[450px] flex flex-wrap justify-evenly ">
+              {comics?.length > 0 ? (
+                comics
+                  .map((comic) => {
+                    return (
+                      <div
+                        key={comic.id}
+                        className="xl:w-[250px] h-[400px] m-4 flex flex-col items-center justify-evenly  ShadowEffect hover:scale-110 duration-1000 overflow-hidden rounded-lg "
+                      >
+                        <img
+                          className="w-full h-72"
+                          src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+                        />
+                        <div className="w-full h-20 bg-gray-700 hover:bg-Marvel duration-1000 text-white capitalize flex justify-center items-center">
+                          {" "}
+                          <Link to={`/ComicDetail/${comic.id}`}>
+                            <p className="text-white text-center text-md ShadowEffect2  ">
+                              {comic.title}
+                            </p>
+                          </Link>
+                        </div>
+                      </div>
+                    );
+                  })
+                  .slice(firstIndex, lastIndex)
+              ) : (
+                <div className="w-3/5 bg-Marvel flex justify-center items-center  ">
+                  <h2 className="text-white font-bold text-center xl:text-6xl uppercase ShadowEffect2">
+                    There are no comics now, try later
+                  </h2>
+                </div>
+              )}
+            </div>
+
+            <Pagination
+              comicsPerPage={comicsPerPage}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+              totalComics={totalComics}
+            />
+          </div>
+        ) : null}
       </div>
     </>
   );
